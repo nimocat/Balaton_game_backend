5538f90ba144c8df87bec59c35e6e31e3c3c7781
nimocat <aitianyi0104@outlook.com>
2024-06-08 15:11:34 +0800

    deleted the main.py's background task

--- main.py
+++ main.py
@@ -15,26 +15,6 @@ from utils.pre_loads import load_data_from_files
 # CURRENT_GAME_SCORES - zset
 # CURRENT_GAME_POOL - int
 
-def clear_reward_ranking_day():
-    redis_client.delete("REWARD_RANKING_DAY")
-
-def schedule_clear_task():
-    while True:
-        now = datetime.utcnow()
-        next_run = datetime(now.year, now.month, now.day) + timedelta(days=1)
-        sleep_time = (next_run - now).total_seconds()
-        time.sleep(sleep_time)
-        clear_reward_ranking_day()
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    task = asyncio.create_task(schedule_clear_task())
-    try:
-        yield
-    finally:
-        task.cancel()
-        await task
-
 app = FastAPI()
 app.include_router(router, prefix="/api/v1/game")
 app.include_router(admin, prefix="/api/v1/admin")
