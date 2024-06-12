28429aa99934cb205ef7d14e077dd6c9425a13f0
nimocat <aitianyi0104@outlook.com>
2024-06-13 00:57:57 +0800

    Daily login count +

--- hooks/player_hook.py
+++ hooks/player_hook.py
@@ -6,11 +6,25 @@ def login_hook(player_name: str):
     # update_daily_task(player_name)
 
 def update_last_login_time(player_name: str):
-    db.players.update_one(
-    {"name": player_name},
-    {"$set": {"last_login_time": datetime.datetime.utcnow()}},
-    upsert=True
-)
+    player_data = db.players.find_one({"name": player_name})
+    current_time = datetime.datetime.utcnow()
+    last_login_time = player_data.get('last_login_time') if player_data else None
+    
+    if last_login_time is None or last_login_time.date() < current_time.date():
+        # It's a new day since the last login
+        new_total_login_days = player_data.get('total_login_days', 0) + 1 if player_data else 1
+        db.players.update_one(
+            {"name": player_name},
+            {"$set": {"last_login_time": current_time, "total_login_days": new_total_login_days}},
+            upsert=True
+        )
+    else:
+        # Update only the last login time
+        db.players.update_one(
+            {"name": player_name},
+            {"$set": {"last_login_time": current_time}},
+            upsert=True
+        )
     
 def update_daily_task(player_name: str):
     if not db.players.find_one({"name": player_name, "daily_tasks.id": 1}):
